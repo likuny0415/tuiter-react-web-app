da0612462679f8f3e3c871b96866bdcfec4eeb24
likuny0415 <li.kuny@husky.neu.edu>
2022-10-21 15:38:30 -0400

    finish lab

---
+++ src/tuiter/navigation-sidebar/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+const NavigationSidebar = () => {
+    return (
+        <div className="list-group">
+          <a className="list-group-item">Tuiter</a>
+          <a className="list-group-item">Home</a>
+          <a className="list-group-item">Explore</a>
+          <a className="list-group-item">Notifications</a>
+          <a className="list-group-item">Messages</a>
+          <a className="list-group-item">Bookmarks</a>
+          <a className="list-group-item">Lists</a>
+          <a className="list-group-item">Profile</a>
+          <a className="list-group-item">More</a>
+        </div>
+       );   
+};
+export default NavigationSidebar;
