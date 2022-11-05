5a7cce75cf6d6809e9dee9e3f85eb209d32976ff
likuny0415 <li.kuny@husky.neu.edu>
2022-11-04 23:20:19 -0400

    push

--- src/tuiter/tuit-list/index.js
+++ src/tuiter/tuit-list/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import {useSelector} from "react-redux";
 import TuitListItem
     from "./tuit-item";
-    import '../../vendors/fontawesome/css/all.min.css';
-    import '../../vendors/bootstrap/css/bootstrap.min.css'
+
 
 
 const TuitList = () => {
